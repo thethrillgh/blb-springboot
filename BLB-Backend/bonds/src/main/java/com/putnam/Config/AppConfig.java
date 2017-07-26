98265bcd63dcff2e0a04d12237260a601d05abbb
Derek <derek.mcmaster@yahoo.com>
2017-07-26 14:31:52 -0400

    new server push

---
+++ BLB-Backend/bonds/src/main/java/com/putnam/Config/AppConfig.java
@@ -0,0 +1,42 @@
+package com.putnam.Config;
+
+import java.util.Arrays;
+
+import javax.ws.rs.ext.RuntimeDelegate;
+
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import com.putnam.Rest.LandingPageService;
+
+@Configuration
+public class AppConfig {	
+	@Bean( destroyMethod = "shutdown" )
+	public SpringBus cxf() {
+		return new SpringBus();
+	}
+	
+	@Bean @DependsOn ( "cxf" )
+	public Server jaxRsServer() {
+		JAXRSServerFactoryBean factory = RuntimeDelegate.getInstance().createEndpoint( jaxRsApiApplication(), JAXRSServerFactoryBean.class );
+		factory.setServiceBeans( Arrays.< Object >asList( landingService() ) );
+		factory.setAddress( factory.getAddress() );
+		factory.setProviders( Arrays.< Object >asList( new JsrJsonpProvider() ) );
+		return factory.create();
+	}
+	
+	@Bean 
+	public JaxRsApiApplication jaxRsApiApplication() {
+		return new JaxRsApiApplication();
+	}
+	
+	@Bean 
+	public LandingPageService landingService() {
+		return new LandingPageService();
+	}		
+}
