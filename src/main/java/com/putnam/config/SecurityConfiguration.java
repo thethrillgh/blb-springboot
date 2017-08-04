78c1161fd163a172ca911c7ad2b2bb9eaf6d4327
dmmcmaster <derek.mcmaster@yahoo.com>
2017-08-04 08:23:41 -0400

    security config

--- src/main/java/com/putnam/config/SecurityConfiguration.java
+++ src/main/java/com/putnam/config/SecurityConfiguration.java
@@ -134,7 +134,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .disable()
         .and()
             .authorizeRequests()
-            .antMatchers("/api/register").permitAll()
+            .antMatchers("/api/*").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/account/reset_password/init").permitAll()
