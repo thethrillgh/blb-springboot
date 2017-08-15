76cadbdd4f1a295e8f172d1c8451f78b9dd55e20
dmmcmaster <derek.mcmaster@yahoo.com>
2017-08-15 13:38:52 -0400

    more tests

--- src/test/java/TestUserAccounts.java
+++ src/test/java/TestUserAccounts.java
@@ -23,12 +23,92 @@ public class TestUserAccounts {
     }
 
     @Test
-    public void testUserVars(){
+    public void testFirstName(){
         User user = new User("John", "Doe", "5089993453", "johndoe@gmail.com", "password1", "022657766", "7766", "AAAAAAAAAAAAAAAA", "4 Main Street", "Boston", "MA", "02129", 100.0);
 
         assertEquals("John", user.getFirstname());
 
         assertNotEquals("Alex", user.getFirstname());
+
+        user.setFirstname("James");
+
+        assertNotEquals("John", user.getFirstname());
+
+        assertEquals("James", user.getFirstname());
     }
 
+    @Test
+    public void testLastName(){
+        User user = new User("John", "Doe", "5089993453", "johndoe@gmail.com", "password1", "022657766", "7766", "AAAAAAAAAAAAAAAA", "4 Main Street", "Boston", "MA", "02129", 100.0);
+
+        assertEquals("Doe", user.getLastname());
+
+        assertNotEquals("Smith", user.getLastname());
+
+        user.setLastname("Johnson");
+
+        assertNotEquals("Doe", user.getLastname());
+
+        assertEquals("Johnson", user.getLastname());
+    }
+
+    @Test
+    public void testPhoneNum(){
+        User user = new User("John", "Doe", "5089993453", "johndoe@gmail.com", "password1", "022657766", "7766", "AAAAAAAAAAAAAAAA", "4 Main Street", "Boston", "MA", "02129", 100.0);
+
+        assertEquals("5089993453", user.getPhonenum());
+
+        assertNotEquals("5089990000", user.getPhonenum());
+
+        user.setPhonenum("6177779898");
+
+        assertNotEquals("5089993453", user.getPhonenum());
+
+        assertEquals("6177779898", user.getPhonenum());
+    }
+
+    @Test
+    public void testAcctEmail(){
+        User user = new User("John", "Doe", "5089993453", "johndoe@gmail.com", "password1", "022657766", "7766", "AAAAAAAAAAAAAAAA", "4 Main Street", "Boston", "MA", "02129", 100.0);
+
+        assertEquals("johndoe@gmail.com", user.getAcctemail());
+
+        assertNotEquals("johndoe@yahoo.com", user.getAcctemail());
+
+        user.setAcctemail("johndoe@yahoo.com");
+
+        assertNotEquals("johndoe@gmail.com", user.getAcctemail());
+
+        assertEquals("johndoe@yahoo.com", user.getAcctemail());
+    }
+
+    @Test
+    public void testAcctPass(){
+        User user = new User("John", "Doe", "5089993453", "johndoe@gmail.com", "password1", "022657766", "7766", "AAAAAAAAAAAAAAAA", "4 Main Street", "Boston", "MA", "02129", 100.0);
+
+        assertEquals("password1", user.getAcctpass());
+
+        assertNotEquals("somepassword", user.getAcctpass());
+
+        user.setAcctpass("somepassword");
+
+        assertNotEquals("password1", user.getAcctpass());
+
+        assertEquals("somepassword", user.getAcctpass());
+    }
+
+    @Test
+    public void testAcctSSN(){
+        User user = new User("John", "Doe", "5089993453", "johndoe@gmail.com", "password1", "022657766", "7766", "AAAAAAAAAAAAAAAA", "4 Main Street", "Boston", "MA", "02129", 100.0);
+
+        assertEquals("022657766", user.getAcctssn());
+
+        assertNotEquals("111223333", user.getAcctssn());
+
+        user.setAcctssn("111223333");
+
+        assertNotEquals("022657766", user.getAcctssn());
+
+        assertEquals("111223333", user.getAcctssn());
+    }
 }
