5b2cb70cfccc2f9d3f21f1f86cdac2192ca3c5a4
Ana P Parra Vera <aparravera@apvera-p-ml01.wifi.putnaminv.com>
2017-08-22 11:11:59 -0400

    fixed user mapping

--- src/main/resources/static/components/signup/signup.js
+++ src/main/resources/static/components/signup/signup.js
@@ -1,15 +1,25 @@
 var signUpController = function($scope, $state){
-    $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+    $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA MA MD ME MI MN MO ' +
+    'MS MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
     'WY').split(' ').map(function(state) {
         return {abbrev: state};
       });
     $scope.signup = function(form){
         if(form){
             var data = {
+                firstname: $scope.firstname,
+                lastname: $scope.lastname,
+                phonenum: $scope.phonenum,
                 acctemail: $scope.email,
                 acctssn: $scope.ssn,
-                acctpass: $scope.password
+                acctpass: $scope.password,
+                acctssn: $scope.acctssn,
+                ssnlastfour: $scope.acctssn.substr($scope.acctssn.length - 4),
+                streetaddress: $scope.streetaddress,
+                city: $scope.city,
+                state: $scope.state,
+                postalcode: $scope.postalcode,
+                acctbalance: 0
             }
             apiService.signup(data).then(function(data){
                 if(data.data.status=="Done"){
