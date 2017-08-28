030523350f075f4a9143b43fb1a75d8f12c7c9d1
yussif hamis <potentialsysm@gmail.com>
2017-08-28 11:48:47 -0400

    favicon

--- src/main/java/com/putnam/controller/BankController.java
+++ src/main/java/com/putnam/controller/BankController.java
@@ -1,6 +1,9 @@
 package com.putnam.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +12,8 @@ import com.putnam.model.Bank;
 import com.putnam.model.User;
 import com.putnam.repository.BankAccountRepository;
 import com.putnam.repository.UserRepository;
+import com.putnam.response.Failed;
+import com.putnam.response.Response;
 
 @RestController
 public class BankController {	
@@ -20,25 +25,13 @@ public class BankController {
 	UserRepository userRepo;
 	
 	@RequestMapping(value="/bank/save", method = RequestMethod.GET)
-	public String save() {
-		User user = userRepo.findByUserid(1);
-		User user1 = userRepo.findByUserid(2);
-		User user2 = userRepo.findByUserid(3);
-		User user3 = userRepo.findByUserid(4);
-		User user4 = userRepo.findByUserid(5);
-		User user5 = userRepo.findByUserid(6);
-		User user6 = userRepo.findByUserid(7);
-		User user7 = userRepo.findByUserid(8);
-		bankRepo.save(new Bank("123456789876","123456789","Savings", user));
-		bankRepo.save(new Bank("394857209345","938398477","Checking", user1));
-		bankRepo.save(new Bank("483539845733","457542768","Checking", user2));
-		bankRepo.save(new Bank("435874773097","563701938","Checking", user3));
-		bankRepo.save(new Bank("989435739489","900745822","Checking", user4));
-		bankRepo.save(new Bank("758376245387","234508547","Checking", user5));
-		bankRepo.save(new Bank("587483937777","435202345","Savings", user6));
-		bankRepo.save(new Bank("458739274857","593048534","Savings", user7));
-		return "Done";
-		
+	public Response save(HttpServletRequest request, @RequestBody Bank bank) {
+		User user = userRepo.findByUserid((long) request.getSession().getAttribute("user_id"));
+		if(user != null) {
+			bankRepo.save(new Bank(bank.getAcctnum(), bank.getRoutingnum(), bank.getAccttype(), user));
+			return new Response("Done", user);
+		}
+		return new Response("Failed", new Failed("Not logged in"));
 	}
 	
 	@RequestMapping(value="/bank/findall", method = RequestMethod.GET)
