666476bb7d86773b1ab8243ec081dff53fded3d6
Issifu Suhununu <issifu.suhununu@centre.edu>
2017-08-28 22:16:34 -0400

    route on click line chart

--- src/main/resources/static/components/explore/explore.js
+++ src/main/resources/static/components/explore/explore.js
@@ -60,15 +60,18 @@ var exploreController = function($scope, $state, $mdEditDialog, $q, $timeout, us
         })
         graphData.push(temp)
   });
-  var array1 = ['data1', 30, 200, 100, 400, 150, 250];
-var array2 = ['data2', 50, 20, 10, 40, 15, 25];
-var array3 = ['data3', 230, 190, 300, 500, 300, 400];
-var Data = [array1, array2, array3];
-console.log(Data)
   setTimeout(function(){
       var chart = c3.generate({
         data: {
-            columns: graphData
+            columns: graphData,
+            onclick: function (d, i) { 
+                console.log(d.name);
+                $scope.mybonds.forEach(function(data){
+                    if(data.cusip == d.name){
+                        $state.go('explorebond', {obj: data})
+                    }
+                })
+            }
         },
         axis:{
             y: {
