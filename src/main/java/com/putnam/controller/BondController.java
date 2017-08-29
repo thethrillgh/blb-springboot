e8758c00acbddb823991605970d7172b3bd65b14
dmmcmaster <derek.mcmaster@yahoo.com>
2017-08-28 23:01:36 -0400

    recommend top bond

--- src/main/java/com/putnam/controller/BondController.java
+++ src/main/java/com/putnam/controller/BondController.java
@@ -1,5 +1,7 @@
 package com.putnam.controller;
 
+import com.putnam.repository.UserRepository;
+import com.putnam.response.Failed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,19 +12,25 @@ import com.putnam.model.Bond;
 import com.putnam.repository.BondRepository;
 import com.putnam.response.Response;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 @RestController
-public class BondController {	
+public class BondController {
+
 	@Autowired
 	BondRepository bondRepo;
 
-	
+	@Autowired
+	UserRepository userRepo;
+
 	@RequestMapping("/save")
 	public String process(){
 		return "Done";
 	}
-	
-	
+
 	@RequestMapping("/findall")
 	public Response findAll(){
 		Iterable<Bond> result = bondRepo.findAll();
@@ -52,8 +60,28 @@ public class BondController {
 		return "Done";
 	}
 
-//	@RequestMapping(value = "/findbest", method = RequestMethod.GET)
-//	public Response getTopPerformingBond(){
-//
-//	}
+	@RequestMapping(value = "/findbest", method = RequestMethod.GET)
+	public Response getTopPerformingBond(){
+
+		long id = -1;
+		double max = 0.00;
+
+		Iterable<Bond> bonds = bondRepo.findAll();
+
+		for(Bond curr: bonds){
+			if(curr.getYieldask() > max){
+				id = curr.getBondid();
+				max = curr.getYieldask();
+			}
+		}
+
+		Bond bond = bondRepo.findByBondid(id);
+
+		if(bond != null){
+			return new Response("Success", bond);
+		}
+
+		return new Response("Fail", new Failed("Problem finding best bond"));
+	}
+
 }
