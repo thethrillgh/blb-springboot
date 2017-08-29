829da0488c56cc399105898aff8f34e9dca187fe
Issifu Suhununu <issifu.suhununu@centre.edu>
2017-08-28 22:04:06 -0400

    experimenting with full chart on explore bonds

--- src/main/resources/static/components/explore/explore.js
+++ src/main/resources/static/components/explore/explore.js
@@ -33,7 +33,7 @@ var exploreController = function($scope, $state, $mdEditDialog, $q, $timeout, us
   
   $scope.query = {
     order: 'cusip',
-    limit: 10 ,
+    limit: 5 ,
     page: 1
   };
   
@@ -51,6 +51,41 @@ var exploreController = function($scope, $state, $mdEditDialog, $q, $timeout, us
         };
     }, 2000);
   }
+  var graphData = [];
+  $scope.mybonds.forEach(function(data){
+        var item = data.history;
+        var temp = [data.cusip];
+        item.forEach(function(data1){
+            temp.push(data1.yieldask.toFixed(2))
+        })
+        graphData.push(temp)
+  });
+  var array1 = ['data1', 30, 200, 100, 400, 150, 250];
+var array2 = ['data2', 50, 20, 10, 40, 15, 25];
+var array3 = ['data3', 230, 190, 300, 500, 300, 400];
+var Data = [array1, array2, array3];
+console.log(Data)
+  setTimeout(function(){
+      var chart = c3.generate({
+        data: {
+            columns: graphData
+        },
+        axis:{
+            y: {
+                label: {
+                    text: 'Yield %',
+                    position: 'outer-right'
+                },
+                tick: {
+                    format: d3.format('.2f')
+                }
+            },
+            x: {
+                show: false
+            }
+        }
+      });
+  }, 200)
 };
 
 
